20cc2058d3c117ef5b7ddff4e49e60ce17fce39d
dangvanphucnghia <nghia812014@gmail.com>
2025-07-14 09:34:52 +0700

    add exception, response by Nghia

--- src/main/java/com/phucnghia/backend_sercurity/configuration/AppConfig.java
+++ src/main/java/com/phucnghia/backend_sercurity/configuration/AppConfig.java
@@ -1,12 +1,1 @@
-package com.phucnghia.backend_sercurity.configuration;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
-@Configuration
-@Profile("!prod")
-@RequiredArgsConstructor
-public class AppConfig {
-    private final UserService userService;
-}
